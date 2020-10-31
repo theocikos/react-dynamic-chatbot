36f8f0cb38259a2d105750c0c0dfeb69df0cd7f5
Theodosis Koutsias <theodosis.koutsias@sbtechnologies.org>
2020-10-31 18:59:48 +0200

    add custom layout

---
+++ src/Components/ChatLayout.jsx
@@ -0,0 +1,19 @@
+import React, { Component, Fragment } from 'react'
+import PropTypes from 'prop-types';
+import styles from '../styles.module.css';
+
+class Header extends Component {
+    render() {
+        return (
+            <Fragment>
+                <div style={{width: '200px', height: '500px'}}>
+                    
+                </div>
+            </Fragment>
+        )
+    }
+static propTypes = {
+}
+}
+
+export default Header;
